d5282d8e53803cd255465686aa59450c85e8de6d
www <429373960@qq.com>
2022-03-14 01:23:30 +0800

    add _filesize function

--- Source/JSON/Parser.cpp
+++ Source/JSON/Parser.cpp
@@ -1,1 +1,12 @@
 #include <Rainbow3D.h>
+#include <Windows.h>
+#include <cstdint>
+namespace {
+	int64_t _filesize(const wchar_t*file) {
+		auto _handle = CreateFile2(file, GENERIC_READ, 0, OPEN_EXISTING, NULL);
+		LARGE_INTEGER _para;
+		GetFileSizeEx(_handle, &_para);
+		int64_t num = _para.QuadPart;
+		return num;
+	}
+}
