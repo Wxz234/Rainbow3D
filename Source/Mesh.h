322033834ddbb007e02cabb3821cb58cdc790b80
www <429373960@qq.com>
2022-03-24 04:07:19 +0800

    update Mesh

--- Source/Mesh.h
+++ Source/Mesh.h
@@ -1,8 +1,17 @@
 #pragma once
 #include <vector>
+#include <cstdint>
+#include "Vector.h"
 class Mesh {
 public:
 	Mesh() {}
+	Mesh(const Mesh&) = default;
+	Mesh(Mesh&&) noexcept = default;
+	Mesh& operator=(const Mesh&) = default;
+	Mesh& operator=(Mesh&&) noexcept = default;
 private:
-	std::vector<int> vertices;
+	std::vector<Vector3> vertices;
+	std::vector<Vector3> normals;
+	std::vector<Vector2> uv;
+	std::vector<uint32_t> indices;
 };
---
+++ Source/Vector.h
@@ -0,0 +1,24 @@
+#pragma once
+
+struct Vector2 {
+	Vector2() { x = y = 0.f; }
+	Vector2(float x, float y, float z) :x(x), y(y) {}
+	Vector2(const Vector2&) = default;
+	Vector2(Vector2&&) noexcept = default;
+	Vector2& operator=(const Vector2&) = default;
+	Vector2& operator=(Vector2&&) noexcept = default;
+	float x;
+	float y;
+};
+
+struct Vector3 {
+	Vector3() { x = y = z = 0.f; }
+	Vector3(float x, float y, float z) :x(x), y(y), z(z) {}
+	Vector3(const Vector3&) = default;
+	Vector3(Vector3&&) noexcept = default;
+	Vector3& operator=(const Vector3&) = default;
+	Vector3& operator=(Vector3&&) noexcept = default;
+	float x;
+	float y;
+	float z;
+};
