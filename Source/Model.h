c95601c7bba2d7773e3c16f3bd411f38f0e022e9
www <429373960@qq.com>
2022-04-06 18:31:41 +0800

    remove model

--- Source/Model.h
+++ Source/Model.h
@@ -1,19 +1,1 @@
 #pragma once
-#include "Device.h"
-class Model {
-public:
-	virtual ~Model() {}
-};
-
-class GLTFModel :public Model {
-public:
-	GLTFModel() {}
-	GLTFModel(const GLTFModel&) = default;
-	GLTFModel(GLTFModel&&) noexcept = default;
-	GLTFModel& operator=(const GLTFModel&) = default;
-	GLTFModel& operator=(GLTFModel&&) noexcept = default;
-private:
-};
-
-GLTFModel* CreateGLTFModelFromFile(const wchar_t* file);
-void DestroyModel(Model* model);
