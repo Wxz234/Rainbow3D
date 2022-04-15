ee83dc074d900fe360a409ff66f0ce8688723df3
www <429373960@qq.com>
2022-04-16 02:39:34 +0800

    Update Json.h

--- Source/Json.h
+++ Source/Json.h
@@ -1,6 +1,14 @@
 #pragma once
 namespace Rainbow3D {
-	class JsonObject {
+	class JsonObject final {
+	public:
+		JsonObject() noexcept {}
+		~JsonObject() {}
+		JsonObject(const JsonObject&) = default;
+		JsonObject(JsonObject&&) noexcept = default;
+		JsonObject& operator=(const JsonObject&) = default;
+		JsonObject& operator=(JsonObject&&) noexcept = default;
+	private:
 
 	};
 }
