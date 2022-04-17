fa6310c0646bd99fcfa5b1f5f3f7938649414139
www <429373960@qq.com>
2022-04-18 02:33:21 +0800

    update gltf

---
+++ Source/Runtime/Model/Model.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Runtime/Object/Object.h"
+namespace Rainbow3D {
+	class Model : public RObject {
+	public:
+
+	};
+}
---
+++ Source/Runtime/Model/glTF.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Runtime/Model/Model.h"
+namespace Rainbow3D {
+	class glTFModel : public Model {
+	public:
+
+	};
+}
