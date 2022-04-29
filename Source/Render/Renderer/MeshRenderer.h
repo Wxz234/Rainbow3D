80a8e20dcb9005c875c08f91a0b0dd52770c6635
www <429373960@qq.com>
2022-04-29 23:36:15 +0800

    update meshrenderer

--- Source/Render/Renderer/MeshRenderer.h
+++ Source/Render/Renderer/MeshRenderer.h
@@ -1,1 +1,13 @@
 #pragma once
+#include "Render/Renderer/Renderer.h"
+#include "Runtime/Mesh/Mesh.h"
+namespace Rainbow3D {
+	class MeshRenderer : public Renderer {
+	public:
+		DISABLE_COPY_AND_ASSIGN(MeshRenderer)
+
+		MeshRenderer(GraphcisDevice* device, Mesh* mesh) :Renderer(device), _mesh(mesh) {}
+	private:
+		Mesh* _mesh;
+	};
+}
--- Source/Render/Renderer/Renderer.h
+++ Source/Render/Renderer/Renderer.h
@@ -1,1 +1,13 @@
 #pragma once
+#include "Core/CoreDef.h"
+#include "Render/Device/GraphicsDevice.h"
+namespace Rainbow3D {
+	class Renderer {
+	public:
+		DISABLE_COPY_AND_ASSIGN(Renderer)
+
+		Renderer(GraphcisDevice* device) :_device(device) {}
+	private:
+		GraphcisDevice* _device;
+	};
+}
