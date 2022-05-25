51b16a23f554cd8b86abaed2d6ac6a62480c9089
www <429373960@qq.com>
2022-05-25 18:55:22 +0800

    update utility

--- Source/Render/Graphics/Utility.cpp
+++ Source/Render/Graphics/Utility.cpp
@@ -5,6 +5,10 @@ namespace Rainbow3D {
 	void Utility::Init() {
 		m_Device->CreateVertexShader(g_vs_r, sizeof(g_vs_r), nullptr, &m_VertexShader);
 		m_Device->CreatePixelShader(g_ps_r, sizeof(g_ps_r), nullptr, &m_PixelShader);
+
+		m_swapChain->GetBuffer(0, IID_PPV_ARGS(&m_texture));
+		m_Device->CreateRenderTargetView(m_texture.Get(), NULL, &m_rtv);
+
 		const D3D11_INPUT_ELEMENT_DESC inputLayout[2] = {
 			{"POSITION", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0},
 			{"TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 16, D3D11_INPUT_PER_VERTEX_DATA, 0 }
@@ -12,13 +16,9 @@ namespace Rainbow3D {
 
 		m_Device->CreateInputLayout(inputLayout, 2, g_vs_r, sizeof(g_vs_r), &m_pVertexLayout);
 
-		m_Context->VSSetShader(m_VertexShader.Get(), nullptr, 0);
-		m_Context->PSSetShader(m_PixelShader.Get(), nullptr, 0);
-
 		CD3D11_SAMPLER_DESC sampler_Desc(D3D11_DEFAULT);
 		m_Device->CreateSamplerState(&sampler_Desc, &m_sampler);
-		m_Context->PSSetSamplers(0, 1, m_sampler.GetAddressOf());
-
+	
 		float point[3][6] = {
 			{ -1.f, -1.f, .1f, 1.f, 0.f, 1.f},
 			{ -1.f,  3.f, .1f, 1.f, 0.f,-1.f},
@@ -29,15 +29,30 @@ namespace Rainbow3D {
 		InitData.pSysMem = point;
 		m_Device->CreateBuffer(&buffer_Desc, &InitData, m_VS_vertex_Buffers.GetAddressOf());
 
-		uint32 stride = 24;
+		DXGI_SWAP_CHAIN_DESC swapchain_desc{};
+		m_swapChain->GetDesc(&swapchain_desc);
+	}
+
+	void Utility::DrawTexture(ID3D11ShaderResourceView* srv) {
+		m_Context->VSSetShader(m_VertexShader.Get(), nullptr, 0);
+		m_Context->PSSetShader(m_PixelShader.Get(), nullptr, 0);
+		m_Context->PSSetSamplers(0, 1, m_sampler.GetAddressOf());
+
+		UINT stride = 24;
 		UINT offset = 0;
 
 		m_Context->IASetVertexBuffers(0, 1, m_VS_vertex_Buffers.GetAddressOf(), &stride, &offset);
 		m_Context->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 		m_Context->IASetInputLayout(m_pVertexLayout.Get());
+
 		DXGI_SWAP_CHAIN_DESC swapchain_desc{};
 		m_swapChain->GetDesc(&swapchain_desc);
 		CD3D11_VIEWPORT viewport(0.f, 0.f, swapchain_desc.BufferDesc.Width, swapchain_desc.BufferDesc.Height);
 		m_Context->RSSetViewports(1, &viewport);
+
+		m_Context->PSSetShaderResources(0, 1, &srv);
+		ID3D11RenderTargetView* plist[] = { m_rtv.Get() };
+		m_Context->OMSetRenderTargets(1, plist, nullptr);
+		m_Context->Draw(3, 0);
 	}
 }
--- Source/Render/Graphics/Utility.h
+++ Source/Render/Graphics/Utility.h
@@ -14,12 +14,21 @@ namespace Rainbow3D {
 
 		void Init();
 
+		void DrawTexture(ID3D11ShaderResourceView* srv);
+
 		Microsoft::WRL::ComPtr<ID3D11Device> m_Device;
 		Microsoft::WRL::ComPtr<ID3D11DeviceContext> m_Context;
 		Microsoft::WRL::ComPtr<IDXGISwapChain> m_swapChain;
 
 		Microsoft::WRL::ComPtr<ID3D11VertexShader> m_VertexShader;
 		Microsoft::WRL::ComPtr<ID3D11PixelShader> m_PixelShader;
+
+		Microsoft::WRL::ComPtr<ID3D11InputLayout> m_pVertexLayout;
+		Microsoft::WRL::ComPtr<ID3D11SamplerState> m_sampler;
+		Microsoft::WRL::ComPtr<ID3D11Buffer> m_VS_vertex_Buffers;
+
+		Microsoft::WRL::ComPtr<ID3D11RenderTargetView> m_rtv;
+		Microsoft::WRL::ComPtr<ID3D11Texture2D> m_texture;
 	};
 
 	inline UniquePtr<Utility> CreateUtility(ID3D11Device* device, IDXGISwapChain* swapchain) {
