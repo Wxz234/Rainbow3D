89bf2c246cb2eb81b837f11fa730e9de699e7112
www <429373960@qq.com>
2022-06-16 00:39:29 +0800

    CreateDevice()

--- Editor/Editor.cpp
+++ Editor/Editor.cpp
@@ -8,6 +8,7 @@ void Draw() {
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow) {
     constexpr uint32 w = 800, h = 600;
     auto window = CreateRenderWindow(L"RainbowEditor", w, h);
+    auto device = CreateDevice();
     window->Show();
     return window->RunLoop(Draw);
 }
--- Include/Graphics/Device.h
+++ Include/Graphics/Device.h
@@ -6,8 +6,14 @@
 namespace Rainbow3D{
     class Device {
     public:
-        Device(){}
+        Device(){
+            D3D12CreateDevice(nullptr, D3D_FEATURE_LEVEL_12_0, __uuidof(**(&m_device)), &m_device);
+        }
     private:
         Microsoft::WRL::ComPtr<ID3D12Device6> m_device;
     };
+
+    inline std::unique_ptr<Device> CreateDevice() {
+        return std::unique_ptr<Device>(new Device);
+    }
 }
