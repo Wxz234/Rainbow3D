b967b74a761321e0e2ef5480b02769e4be205e9f
www <429373960@qq.com>
2022-06-16 00:12:34 +0800

    Update Device.h

--- Include/Graphics/Device.h
+++ Include/Graphics/Device.h
@@ -1,9 +1,13 @@
 #pragma once
 #include "CorePreDef.h"
 #include <d3d12.h>
+#include <wrl/client.h>
 
 namespace Rainbow3D{
     class Device {
     public:
+        Device(){}
+    private:
+        Microsoft::WRL::ComPtr<ID3D12Device6> m_device;
     };
 }
