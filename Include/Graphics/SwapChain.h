c5b69d3f0d9c57eebc985cc674e8557a7fede92f
www <429373960@qq.com>
2022-06-16 22:57:59 +0800

    update RTV

--- Include/Graphics/SwapChain.h
+++ Include/Graphics/SwapChain.h
@@ -2,6 +2,7 @@
 #include "CorePreDef.h"
 #include "Platform/Window.h"
 #include "Graphics/Device.h"
+#include <d3d12.h>
 #include <dxgi1_6.h>
 #include <wrl/client.h>
 
@@ -33,6 +34,24 @@ namespace Rainbow3D {
 
             m_factory->CreateSwapChainForHwnd(queue, hwnd, &_desc, &fsSwapChainDesc, nullptr, &m_swapchain);
             m_factory->MakeWindowAssociation(hwnd, DXGI_MWA_NO_WINDOW_CHANGES | DXGI_MWA_NO_ALT_ENTER);
+
+            Microsoft::WRL::ComPtr<ID3D12Device> m_device;
+            queue->GetDevice(__uuidof(**(&m_device)), &m_device);
+            D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc = {};
+            rtvHeapDesc.NumDescriptors = FRAME_NUM;
+            rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+            rtvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+            m_device->CreateDescriptorHeap(&rtvHeapDesc, __uuidof(**(&m_rtvHeap)), &m_rtvHeap);
+            m_rtvDescriptorSize = m_device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+
+            auto rtvHandle = m_rtvHeap->GetCPUDescriptorHandleForHeapStart();
+            for (uint32 n = 0; n < FRAME_NUM; ++n)
+            {
+                Microsoft::WRL::ComPtr<ID3D12Resource> m_renderTargets;
+                m_swapchain->GetBuffer(n, __uuidof(**(&m_renderTargets)), &m_renderTargets);
+                m_device->CreateRenderTargetView(m_renderTargets.Get(), nullptr, rtvHandle);
+                rtvHandle.ptr = rtvHandle.ptr + m_rtvDescriptorSize;
+            }
         }
 
         void Present(uint32 sync) {
@@ -42,9 +61,17 @@ namespace Rainbow3D {
         IDXGISwapChain* GetSwapChain() const {
             return m_swapchain.Get();
         }
+
+        D3D12_CPU_DESCRIPTOR_HANDLE GetRTV(uint32 n) const {
+            auto rtvHandle = m_rtvHeap->GetCPUDescriptorHandleForHeapStart();
+            rtvHandle.ptr = rtvHandle.ptr + n * m_rtvDescriptorSize;
+            return rtvHandle;
+        }
     private:
         Microsoft::WRL::ComPtr<IDXGIFactory7> m_factory;
         Microsoft::WRL::ComPtr<IDXGISwapChain1> m_swapchain;
+        Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> m_rtvHeap;
+        uint32 m_rtvDescriptorSize;
     };
 
     inline std::unique_ptr<SwapChain> CreateSwapChain(Device* device, RWindow* window, uint32 w, uint32 h) {
