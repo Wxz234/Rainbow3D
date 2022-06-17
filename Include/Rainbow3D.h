7780dc5723b5b306718ac120018f209ff773572d
www <429373960@qq.com>
2022-06-18 02:57:59 +0800

    add camera

--- Include/Rainbow3D.h
+++ Include/Rainbow3D.h
@@ -4,3 +4,7 @@
 #include "Graphics/Device.h"
 #include "Graphics/SwapChain.h"
 #include "Graphics/RootSignature.h"
+#include "Runtime/Component/Component.h"
+#include "Runtime/Animation/Animation.h"
+#include "Runtime/GameObject/GameObject.h"
+#include "Runtime/GameObject/CameraObject.h"
