4bf8f717553a8ee42498fe0996f633efeb0cf3ff
www <429373960@qq.com>
2022-06-19 02:54:40 +0800

    Update ModelObject.cpp

--- Runtime/ModelObject.cpp
+++ Runtime/ModelObject.cpp
@@ -1,7 +1,18 @@
+#define TINYGLTF_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#define TINYGLTF_USE_CPP14
 #include "Runtime/GameObject/ModelObject.h"
+#include "tiny_gltf.h"
 #include <filesystem>
 namespace Rainbow3D {
 	std::unique_ptr<ModelObject> CreateModelObject(const wchar_t* file) {
+		tinygltf::Model model;
+		tinygltf::TinyGLTF loader;
+		std::string err;
+		std::string warn;
+		std::filesystem::path my_path{ file };
+		bool ret = loader.LoadASCIIFromFile(&model, &err, &warn, my_path.string());
 
 		return nullptr;
 	}
