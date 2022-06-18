c781bf745692298a235c58e923a8f3396965d1b7
www <429373960@qq.com>
2022-06-19 02:39:04 +0800

    update CreateModelObject function

--- Include/Runtime/GameObject/ModelObject.h
+++ Include/Runtime/GameObject/ModelObject.h
@@ -1,5 +1,10 @@
 #pragma once
+#include "CorePreDef.h"
 #include "Runtime/GameObject/GameObject.h"
 namespace Rainbow3D {
+	class ModelObject : public GameObject {
+	public:
+	};
 
+	std::unique_ptr<ModelObject> CreateModelObject(const wchar_t*file);
 }
--- Runtime/ModelObject.cpp
+++ Runtime/ModelObject.cpp
@@ -1,1 +1,8 @@
-#include "Runtime/GameObject/ModelObject.h""
+#include "Runtime/GameObject/ModelObject.h"
+#include <filesystem>
+namespace Rainbow3D {
+	std::unique_ptr<ModelObject> CreateModelObject(const wchar_t* file) {
+
+		return nullptr;
+	}
+}
