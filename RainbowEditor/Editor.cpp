a27ca614a7603c92ef337c59b98a84678295a36d
Wxz <429373960@qq.com>
2022-06-22 03:06:40 +0800

    update editor

--- RainbowEditor/Editor.cpp
+++ RainbowEditor/Editor.cpp
@@ -1,4 +1,5 @@
-
-int main() {
-
+#include <Windows.h>
+int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow)
+{
+    return MessageBox(NULL, L"hello, world", L"caption", 0);
 }
